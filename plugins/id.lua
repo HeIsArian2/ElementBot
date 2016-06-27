498f1c455e975a49ca19a1f39b15c731a27cc609
AllwenWaker <AllwenWaker81@gmail.com>
2016-06-27 10:54:32 -0700

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -3,13 +3,13 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 
-local info = '~This Is Your & Gp Info \n~Your Name: '..msg.from.first_name..'\n'
-..'~Your Id : '..msg.from.id..'\n'
-..'~Your Username : @'..msg.from.username..'\n'
-..'~Group Id : '..msg.to.id..'\n'
-..'~Group name : '..msg.to.title..'\n'
-..'~Your Special Link : https://telegram.me/'..msg.from.username..'\n'
-..'`Your Phone : +'..msg.from.phone
+local info = '~This Is Your & Gp Info !\n~Your Name: -> '..msg.from.first_name..'\n'
+..'~Your Id : -> '..msg.from.id..'\n'
+..'~Your Username : -> @'..msg.from.username..'\n'
+..'~Group Id : -> '..msg.to.id..'\n'
+..'~Group name : -> '..msg.to.title..'\n'
+..'~Your Special Link : -> https://telegram.me/'..msg.from.username..'\n'
+..'~Your Phone : +'..msg.from.phone
 
 reply_msg(reply_id, info, ok_cb, false)
 end
