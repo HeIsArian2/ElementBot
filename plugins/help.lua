82881c2824126d1163d23f4385758e6748174b5d
AllwenWaker <AllwenWaker81@gmail.com>
2016-06-28 00:49:37 -0700

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 if matches[1]:lower() == 'help>' then
-   send_document(get_receiver(msg), "./StarBot/data/help.html", ok_cb, false)
+   send_document(get_receiver(msg), "./ElementBot/data/help.html", ok_cb, false)
 end
 end
 return {
