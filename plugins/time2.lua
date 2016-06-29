b6e860aac9f7dd407e5827b2c33d138a522ba01d
AllwenWaker <AllwenWaker81@gmail.com>
2016-06-29 05:36:30 -0700

    Update time2.lua

--- plugins/time2.lua
+++ plugins/time2.lua
@@ -1,10 +1,17 @@
 function run(msg, matches)
-local url = http.request('http://api.gpmod.ir/time/')
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
- local text =  '🕒 ساعت '..jdat.FAtime..' \n📆 امروز '..jdat.FAdate..' میباشد.\n    —--\n🕒 '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n\n@cannel_window'
-return text
+local stickurl = 'http://latex.codecogs.com/png.download?'
+	..'\\dpi{300}%20\\LARGE%20'..jdat.ENtime
+local file = download_to_file(stickurl,'time.webp')
+send_document('channel#id'..msg.to.id,file,ok_cb,false)
+return 'امروز📆:\n'..jdat.FAdate
 end
 return {
-  patterns = {"^[#/!]([Tt][iI][Mm][Ee])$"}, 
-run = run, 
+  patterns = {"^[/!]([Tt][iI][Mm][Ee])$"}, 
+run = run 
 }
+
+--Created By Butler TM 
+--@BUTLERTG
